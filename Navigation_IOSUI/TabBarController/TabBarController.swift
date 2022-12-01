6d58e540bb1c467f146ce70c7797bf9f06199b49
Olga Tsaturova <miss_olga@inbox.ru>
2022-12-01 17:00:19 +0300

    Home work 5

--- Navigation_IOSUI/TabBarController/TabBarController.swift
+++ Navigation_IOSUI/TabBarController/TabBarController.swift
@@ -7,27 +7,29 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
-    var firstTabNavigationController : UINavigationController!
-    var secondTabNavigationControoller : UINavigationController!
+final class TabBarController: UITabBarController {
+    
+    var feedTabNavigationController: UINavigationController!
+    var profileTabNavigationController: UINavigationController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
+    
     private func setupUI() {
-        firstTabNavigationController = UINavigationController.init(rootViewController: FeedViewController())
-        secondTabNavigationControoller = UINavigationController.init(rootViewController: ProfileViewController())
+        profileTabNavigationController = UINavigationController.init(rootViewController: LoginViewController())
+        feedTabNavigationController = UINavigationController.init(rootViewController: FeedViewController())
         
-        self.viewControllers = [firstTabNavigationController, secondTabNavigationControoller]
+        self.viewControllers = [feedTabNavigationController, profileTabNavigationController]
         
-        let item1 = UITabBarItem(title: "Tape", image: UIImage(systemName: "house.fill"), tag: 0)
-        let item2 = UITabBarItem(title: "Profile", image:  UIImage(systemName: "person.fill"), tag: 1)
+        let item1 = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 0)
+        let item2 = UITabBarItem(title: "Feed", image: UIImage(systemName: "note.text"), tag: 1)
         
-        firstTabNavigationController.tabBarItem = item1
-        secondTabNavigationControoller.tabBarItem = item2
+        profileTabNavigationController.tabBarItem = item1
+        feedTabNavigationController.tabBarItem = item2
         
-        UITabBar.appearance().tintColor = UIColor (red: 0/255.0, green: 146/255.0, blue: 248/255.0, alpha: 1.0)
-        UITabBar.appearance().backgroundColor = .systemGray
+        UITabBar.appearance().backgroundColor = .white
     }
 }
+
